e117ae21c135c4f79be8c6a7bc6e7a5300f0834c
Denis Butsky <nthbutsky@gmail.com>
2022-01-24 16:09:33 +0200

    add basic logic and setup vuex for users

---
+++ src/store/modules/users.js
@@ -0,0 +1,70 @@
+export default {
+  state: {
+    allUsers: null,
+    selectedUsers: null,
+    apiUrl: "https://api.github.com/users",
+  },
+
+  getters: {
+    allUsers(state) {
+      return state.allUsers;
+    },
+
+    selectedUsers(state) {
+      return state.selectedUsers;
+    },
+
+    apiUrl(state) {
+      return state.apiUrl;
+    },
+  },
+
+  mutations: {
+    SET_ALL_USERS(state, payload) {
+      state.allUsers = payload;
+    },
+
+    SET_SELECTED_USERS(state, payload) {
+      state.selectedUsers = payload;
+    },
+
+    ADD_SELECTED_USER(state, payload) {
+      state.selectedUsers.push(payload);
+    },
+
+    DELETE_SELECTED_USER(state, payload) {
+      state.selectedUsers = state.selectedUsers.filter(
+        (el) => el.id !== payload
+      );
+    },
+  },
+
+  actions: {
+    async getUsers({ commit, getters }) {
+      try {
+        const response = await fetch(getters.apiUrl);
+        const data = await response.json();
+        console.log("ALL USERS:", data);
+        commit("SET_ALL_USERS", data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    selectUser({ commit, getters }, payload) {
+      const selectedUser = getters.allUsers.filter(
+        (el) => el.id === payload
+      )[0];
+      if (getters.selectedUsers === null) {
+        commit("SET_SELECTED_USERS", []);
+      }
+      if (getters.selectedUsers.find((el) => el.id === selectedUser.payload)) {
+        commit("DELETE_SELECTED_USER", payload);
+      } else {
+        commit("ADD_SELECTED_USER", selectedUser);
+      }
+      console.log(selectedUser);
+      console.log(getters.selectedUsers);
+    },
+  },
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <Main />
+    <Sidebar />
+    <Footer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "../components/Header.vue";
+import Main from "../components/Main.vue";
+import Sidebar from "../components/Sidebar.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Header,
+    Main,
+    Sidebar,
+    Footer,
   },
 };
 </script>
