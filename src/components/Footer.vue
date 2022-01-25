c7c18976e8f5b71d1c89ca76ccdee80471d526d8
Denis Butsky <nthbutsky@gmail.com>
2022-01-25 03:38:00 +0200

    style footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Footer</div>
+  <div class="footer">
+    <a class="footer__link" href="#" target="blank">GitHub Repo</a>
+  </div>
 </template>
 
 <script>
@@ -8,4 +10,28 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.footer {
+  height: 44px;
+  width: 100vw;
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  background: hsl(0, 0%, 4%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__link {
+    color: hsl(0, 0%, 100%);
+    font-size: 1.5rem;
+    text-decoration: none;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+      color: hsl(178, 63%, 39%);
+    }
+  }
+}
+</style>
