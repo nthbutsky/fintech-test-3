20ae2d31d0a1773015445ba03b0e56924ffbb87f
Denis Butsky <nthbutsky@gmail.com>
2022-01-25 03:40:25 +0200

    deploy

---
+++ precache-manifest.e219e94b2dda4e523a54073497735d68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f2009abccee02b2f40",
+    "url": "/fintech-test-3/css/app.a67d1724.css"
+  },
+  {
+    "revision": "3c1aa6fde6e50df984a6",
+    "url": "/fintech-test-3/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "1ec4878c6b0f4613316a11427316a346",
+    "url": "/fintech-test-3/img/bg.1ec4878c.jpg"
+  },
+  {
+    "revision": "22d91280d152af6c08022f4411238df9",
+    "url": "/fintech-test-3/index.html"
+  },
+  {
+    "revision": "b9f2009abccee02b2f40",
+    "url": "/fintech-test-3/js/app.ad76ee13.js"
+  },
+  {
+    "revision": "3c1aa6fde6e50df984a6",
+    "url": "/fintech-test-3/js/chunk-vendors.adcf219a.js"
+  },
+  {
+    "revision": "0d691a332a595518a7d0e67476144eaa",
+    "url": "/fintech-test-3/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fintech-test-3/robots.txt"
+  }
+]);
